64d8bfa0d717ad578fa0e6fbc35df94728a64718
Carlos <cuzikjack@gmail.com>
2019-07-29 14:35:18 -0300

    fix usercpf

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_and_belongs_to_many :projects
   has_many :tasks
 
-  # validates :cpf, uniqueness: { case_sensitive: false }
+  validates :cpf, uniqueness: { case_sensitive: false }
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -17,6 +17,7 @@ class User < ApplicationRecord
       user.expires_at = auth.credentials.expires_at
       user.refresh_token = auth.credentials.refresh_token
       user.email = auth.info.email
+      user.cpf = auth.info.email
       user.name = auth.info.name
       user.password =  SecureRandom.hex(40)
     end
