d62372bf5506695cc9286c84376c9d1dc16ba6d1
Carlos <cuzikjack@gmail.com>
2019-07-31 19:05:45 -0300

    fix layout

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,18 +1,6 @@
 module ProjectsHelper
   # created in_progress late finished
   def class_color_project(project)
-    if project.created?
-      "bg-info"
-    else
-      if project.in_progress?
-        "bg-warning"
-      else
-        if project.late?
-          "bg-danger"
-        else
-          "bg-success"
-        end
-      end
-    end
+    'bg-light'
   end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,20 +1,6 @@
 module TasksHelper
   # created in_progress late finished
   def class_color_task(task)
-    return 'bg-info'
-
-    if task.created?
-      "bg-info"
-    else
-      if task.in_progress?
-        "bg-warning"
-      else
-        if task.late?
-          "bg-danger"
-        else
-          "bg-success"
-        end
-      end
-    end
+    'bg-light'
   end
 end
